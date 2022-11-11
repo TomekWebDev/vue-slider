5d6fe9b28d2000dbd32e8ee672669732c5dfe994
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-11-11 15:35:00 +0100

    base functionality with background colors done

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            sliderBackground: ['bg-primary', 'bg-danger', 'bg-success'],
+            sliderIndex: 0,        
+        },
+        methods: {
+            slideRight: function(){
+                if(this.sliderIndex < this.sliderBackground.length-1){
+                   return this.sliderIndex++
+                }
+                return this.sliderIndex = 0
+            },
+            slideLeft: function(){
+                if(this.sliderIndex > 0){
+                   return this.sliderIndex--
+                }
+                return this.sliderIndex = this.sliderBackground.length-1
+            },
+            
+
+        }
+    }
+)
